ab3eca53e8fde4b83b41cf9102542d808f930632
theophile <theomolinatti@gmail.com>
2020-04-29 17:20:33 +0200

    Mise à jour backup

--- backup.c
+++ backup.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -23,6 +24,7 @@ typedef struct
 {
 	int *memRapide;		//< Memoire physique RAM
 	int *memLente;		//< Memoire physique Disque
+	int *tabIndexe;		//< Indexe pour le LRU
 	
 } MEMORY;
 
@@ -32,7 +34,6 @@ typedef struct
 	
 	int hit;		//< Nombre de hit
 	int numReq;		//< Nombre de requête
-	int start;
 	
 } ARG_T;
 
@@ -107,6 +108,10 @@ MEMORY initMemoirePhysique(int nbrPage, int nbrFrames)
 	for(int size = 0; size < nbrFrames; size++)
 		mem.memRapide[size] = -1;
 	
+	mem.tabIndexe = malloc(nbrFrames * sizeof(int));
+	for(int size = 0; size < nbrFrames; size++)
+		mem.tabIndexe[size] = -1;
+		
 	return mem;
 }
 
@@ -120,17 +125,100 @@ void afficheConfig(DATA dt)
 }
 
 
-//~ int *LRU(int frame)
-//~ {
-	//~ int *cache;
+int LRU(int nbrFrame, int nbrPage, MEMORY *mem, int adresse)
+{
+	int valMax = 0;
 	
-	//~ return cache;
-//~ }
+	//Recheche dans la mémoire Rapide
+	for(int cmptR = 0; cmptR < nbrFrame; cmptR++)
+	{
+		if(mem -> memRapide[cmptR] == -1)
+		{
+			mem -> memRapide[cmptR] = adresse;
+			
+			for(int cmptI = 0; cmptI < nbrFrame; cmptI++)
+			{
+				if(mem -> tabIndexe[cmptI] != -1 && cmptI != cmptR)
+					mem -> tabIndexe[cmptI] += 1;
+				
+				else if(cmptI == cmptR)
+					mem -> tabIndexe[cmptI] = 0;
+			}
+			
+			return 0; //Voir quoi return si rien d'allouer ?
+		}
+		
+		else if(mem -> memRapide[cmptR] == adresse)
+		{
+			for(int cmptI = 0; cmptI < nbrFrame; cmptI++)
+			{
+				if(mem -> tabIndexe[cmptI] != -1 && cmptI != cmptR)
+					mem -> tabIndexe[cmptI] += 1;
+				
+				else if(cmptI == cmptR)
+					mem -> tabIndexe[cmptI] = 0;
+			}
+			
+			return mem -> memRapide[cmptR]; //Pas sur ?
+		}
+	}
+	
+	//Recherche dans la memoire lente
+	for(int cmptL = 0; cmptL < nbrPage; cmptL++)
+	{
+		if(mem -> memLente[cmptL] == adresse)
+		{
+			//Recherche de la postion du plus ancien
+			for(int cmptI = 0; cmptI < nbrFrame; cmptI++)
+			{
+				if(mem -> tabIndexe[cmptI] > valMax)
+					valMax = mem -> tabIndexe[cmptI];
+			}
+			
+			//Modification du cache interversion de la valeur memL avec memR
+			for(int cmptR = 0; cmptR < nbrFrame; cmptR++)
+			{
+				if(mem -> tabIndexe[cmptR] == valMax)
+				{
+					mem -> memLente[cmptL] = mem -> memRapide[cmptR];
+					mem -> memRapide[cmptR] = adresse;
+					
+					//Mise à jour de l'indexe
+					for(int cmptI = 0; cmptI < nbrFrame; cmptI++)
+					{
+						if(mem -> tabIndexe[cmptI] != -1 && cmptI != cmptR)
+							mem -> tabIndexe[cmptI] += 1;
+						
+						else if(cmptI == cmptR)
+							mem -> tabIndexe[cmptI] = 0;
+					}
+					
+					return -1;// Trouver quoi return si le thread va cherhcer l'info dans la memoire lente
+				}	
+			}
+		}
+	}
+	
+	return -1; //Si on arrive ici on a une erreur !
+}
 
 void creationTube()
 {
-	mkfifo("/tmp/FIFO1", 0666);
-	mkfifo("/tmp/FIFO2", 0666);
+	int valFIFO;
+	
+	valFIFO = mkfifo("/tmp/FIFO1", 0666);
+	if(valFIFO != 0)
+	{
+		fprintf(stderr, "\nERR: creating fifo1\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	valFIFO = mkfifo("/tmp/FIFO2", 0666);
+	if(valFIFO != 0)
+	{
+		fprintf(stderr, "\nERR: creating fifo2\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
 void suppressionTube()
@@ -139,7 +227,7 @@ void suppressionTube()
 	unlink("/tmp/FIFO2");
 }
 
-void lectureTube(const char *chemin)
+int lectureTube(const char *chemin)
 {
 	int valRD, nb_numbers;
 	
@@ -155,12 +243,13 @@ void lectureTube(const char *chemin)
 	{
 		fprintf(stderr, "\nERR: read in pipe\n");
 		SE_fermeture(tube);
+		unlink(chemin);
 		exit(EXIT_FAILURE);
 	}
 	
-	printf("Lecture reussite pipe n°2 fils : %d\n", nb_numbers);
-	
 	SE_fermeture (tube);
+	
+	return nb_numbers;
 }
 
 void ecritureTube(const char *chemin, int val)
@@ -179,6 +268,7 @@ void ecritureTube(const char *chemin, int val)
 	{
 		fprintf(stderr, "\nERR: write in pipe\n");
 		SE_fermeture(tube);
+		unlink(chemin);
 		exit(EXIT_FAILURE);
 	}
 	
@@ -187,6 +277,7 @@ void ecritureTube(const char *chemin, int val)
 	{
 		fprintf(stderr, "\nERR: write in pipe\n");
 		SE_fermeture(tube);
+		unlink(chemin);
 		exit(EXIT_FAILURE);
 	}
 	
@@ -197,93 +288,28 @@ void * demandeAcces (void * arg)
 {
 	ARG_T *at = (ARG_T *) arg;
 	
-	int valRD, valWR, nb_numbers;
-	
 	//Création du chemin pour le tube
 	const char *chemin1 = "/tmp/FIFO1";
 	const char *chemin2 = "/tmp/FIFO2";
-			
-	printf("debut du thread fils\n");
-	for(int cmpt = at -> start; cmpt < at -> numReq; cmpt++)
+	
+	int reponse;
+		
+	printf("debut du thread fils : %ld\n", pthread_self());
+	for(int cmpt = 0; cmpt < at -> numReq; cmpt++)
 	{
 		//Faire l'appel à la mutex
 		pthread_mutex_lock(at -> mut);
 		
-		printf("debut du thread fils entrant dans le mutex\n");
+		printf("tid thread fils : %ld\n", pthread_self());
 		
-		//Ouverture du tube en mode ecriture
-		printf("Tentative d'ouverture du tube n°1 par le fils\n");
-		SE_FICHIER tube = SE_ouverture(chemin1, O_WRONLY);
-		if (tube.descripteur < 0) 
-		{
-			fprintf(stderr, "\nERR: opening pipe\n");
-			exit(EXIT_FAILURE);
-		}
-		printf("Ouverture réussite du tube n°1 par le fils\n");
+		ecritureTube(chemin1, cmpt);
 		
-		//Ectiture de la demande dans un tube en mode ecriture
-		sleep(0.1);
-		printf("tid : %ld\n", pthread_self());
-		printf("Tentative d'ecriture pipe n°1 fils\n");
-		
-		valWR = SE_ecritureEntier(tube, pthread_self());
-		if (valWR <= 0) 
-		{
-			fprintf(stderr, "\nERR: write in pipe\n");
-			SE_fermeture(tube);
-			exit(EXIT_FAILURE);
-		}
-		
-		valWR = SE_ecritureCaractere (tube, ' ');
-		if(valWR <= 0)
-		{
-			fprintf(stderr, "\nERR: write in pipe\n");
-			SE_fermeture(tube);
-			exit(EXIT_FAILURE);
-		}
-		
-		printf("Ecriture reussite pipe n°1 fils\n");
-		sleep(0.4);
-		
-		//Fermeture du tube
-		SE_fermeture (tube);
-		printf("Fermeture reussite pipe n°1 fils\n");
-		
-		sleep(0.2);
-		
-		//Ouverture du tube pour la lecture
-		printf("Tentative d'ouveture du pipe n°2 fils\n");
-		tube = SE_ouverture(chemin2, O_RDONLY);
-		if (tube.descripteur < 0) 
-		{
-			fprintf(stderr, "\nERR: opening pipe\n");
-			exit(EXIT_FAILURE);
-		}
-		printf("Ouverture réussite du tube n°2 par le fils\n");
-		
-		//Lecture de la reponse de la part du pere
-		sleep(0.1);
-		printf("Tentative de lecture pipe n°2 fils\n");
-		valRD = SE_lectureEntier(tube, &nb_numbers);
-		if (valRD <= 0) 
-		{
-			fprintf(stderr, "\nERR: read in pipe\n");
-			SE_fermeture(tube);
-			exit(EXIT_FAILURE);
-		}
-		
-		printf("Lecture reussite pipe n°2 fils : %d\n", nb_numbers);
-		
-		//Fermeture et Suppression du tube
-		SE_fermeture (tube);
-		printf("Fermeture du tube n°2 fils\n");
-		
-		unlink(chemin2);
-		printf("Supression du tube n°2 fils\n");
-		
+		reponse = lectureTube(chemin2);
+		if(reponse != -1)
+			at -> hit += 1;
+				
 		//Fin du mutex
 		pthread_mutex_unlock(at -> mut);
-		printf("Fin du mutex\n");
 	}
 	
 	return NULL;
@@ -291,20 +317,25 @@ void * demandeAcces (void * arg)
 
 void gestionThread(DATA dt)
 {
-	pthread_mutex_t mut;
+	pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 	pthread_t * tid;
 	
 	ARG_T *at;
+	MEMORY mem;
+	
+	int adresse, page;
+	int moyenneHit = 0;
 	
 	const char *chemin1 = "/tmp/FIFO1";
 	const char *chemin2 = "/tmp/FIFO2";
-	int valWR, valRD, nb_numbers, valFIFO;
-	
+		
 	at  = malloc(dt.nbrThread * sizeof(ARG_T));
-	tid = malloc (dt.nbrThread * sizeof (pthread_t) );
+	tid = malloc(dt.nbrThread * sizeof(pthread_t) );
 	
 	//Creation mutex et barriere
 	pthread_mutex_init(&mut, NULL);
+	
+	mem = initMemoirePhysique(dt.nbrPage, dt.nbrFrames);
 	
 	//~ //Init des infos pour la structure des threads
 	for(int cmpt = 0; cmpt < dt.nbrThread; cmpt++)
@@ -312,112 +343,37 @@ void gestionThread(DATA dt)
 		at[cmpt].mut = &mut;
 		at[cmpt].hit = 0;
 		at[cmpt].numReq = dt.nbrAcces;
-		at[cmpt].start = 0;
 		pthread_create(tid + cmpt, NULL, demandeAcces, at + cmpt);
 	}
 	
-	//Communication par tubes
-
 	printf("Début du thread père\n");
 	printf("for = %d\n", dt.nbrThread * dt.nbrAcces);
+	
+	creationTube();
+	
 	for(int cmpt = 0; cmpt < dt.nbrThread * dt.nbrAcces; cmpt++)
 	{
-		//Probleme comment connaitre le tid de lecture var global?
-		//Recupération du chemin
-		printf("Creation du thread n°1\n");
-		valFIFO = mkfifo(chemin1, 0600);
-		if (valFIFO) 
-		{
-			fprintf(stderr, "\nERR: creating pipe n°1\n");
-			unlink(chemin1);
-			exit(EXIT_FAILURE);
-		}
+		adresse = lectureTube(chemin1);
 		
-		//Ouverture du tube
-		sleep(0.2);
-		printf("Tentative d'ouverture du tube n°1 par le père\n");
-		SE_FICHIER tube = SE_ouverture(chemin1, O_RDONLY);
-		if (tube.descripteur < 0) 
-		{
-			fprintf(stderr, "\nERR: opening pipe\n");
-			exit(EXIT_FAILURE);
-		}
+		page = LRU(dt.nbrFrames, dt.nbrPage, &mem, adresse);
 		
-		printf("Ouverture réussite du tube n°1 par le père\n");
-		
-		//Lecture du tube avec comme chemin le tid du thread
-		printf("Tentative de lecture du tube n°1 par le pere\n");
-		valRD = SE_lectureEntier(tube, &nb_numbers);
-		if (valRD <= 0) 
-		{
-			fprintf(stderr, "\nERR: missing nb_numbers\n");
-			SE_fermeture(tube);
-			exit(EXIT_FAILURE);
-		}
-		
-		printf("Lecture réussite du pipe père n°1 : %d\n", nb_numbers);
-		
-		//Recuperation de l'information ecrite dans le tube 
-		//Fermeture et Suppresion du tube 
-		SE_fermeture (tube);
-		printf("Fermuture du tube n°1 par le pere\n");
-		
-		unlink(chemin1);
-		printf("Suppression du tube n°1 par le pere\n");
-		
-		//Utiliser l'algo de LRU pout verifier la demande soit presente dans le cache
-		//Creation du tube à partir du même tid que le tube du mode lecture
-		printf("Creation du pipe père n°2\n");
-		valFIFO = mkfifo(chemin2, 0600);
-		if (valFIFO) 
-		{
-			fprintf(stderr, "\nERR: creating pipe n°2\n");
-			unlink(chemin2);
-			exit(EXIT_FAILURE);
-		}
-		
-		//Si la valeur est presente renvoyer la valeur grace au tube en ecriture
-		//Ouverture du tube en mode ecriture
-		printf("Tentative d'ouverture du pipe père n°2\n");
-		tube = SE_ouverture(chemin2, O_WRONLY);
-		if (tube.descripteur < 0) 
-		{
-			fprintf(stderr, "\nERR: opening pipe\n");
-			exit(EXIT_FAILURE);
-		}
-		
-		printf("Ouverture reussite du pipe père n°2\n");
-		
-		//Ecriture dans le tube
-		
-		printf("Tentative d'ecriture pipe n°2 père\n");
-		valWR = SE_ecritureEntier(tube, pthread_self());
-		if (valWR <= 0) 
-		{
-			fprintf(stderr, "\nERR: write in pipe\n");
-			SE_fermeture(tube);
-			exit(EXIT_FAILURE);
-		}
-		
-		valWR = SE_ecritureCaractere (tube, ' ');
-		if(valWR <= 0)
-		{
-			fprintf(stderr, "\nERR: write in pipe\n");
-			SE_fermeture(tube);
-			exit(EXIT_FAILURE);
-		}
-		
-		printf("Ecriture reussite pipe n°2 père\n");
-		//Sinon renvoyé 0 par exemple 
-		
-		//Fermeture du tube
-		SE_fermeture (tube);
-		printf("Fermeture du pipe n°2 père\n");
+		ecritureTube(chemin2, page);
 	}
+	
+	suppressionTube();
 	
 	//Fin des threads
 	for(int cmpt = 0; cmpt < dt.nbrThread; cmpt++)
 		pthread_join(tid[cmpt], NULL);
+	
+	for(int cmptT = 0; cmptT < dt.nbrThread; cmptT++)
+	{
+		printf("Nombre de hit pour le thread n° = %d\n", at[cmptT].hit);
+		moyenneHit += at[cmptT].hit;
+	}
+	
+	moyenneHit /= dt.nbrThread;
+	printf("La moyenne de hit est de : %d\n", moyenneHit);
 	
 	//Suppresion des mutex et des barrieres 
 	pthread_mutex_destroy(&mut);
